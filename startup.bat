c1e54959833bf774deecdb647c925cfc08c7f4fb
Lance8931 <1838133252@qq.com>
2018-04-10 23:49:53 +0800

    update
    sdf

---
+++ startup.bat
@@ -0,0 +1,17 @@
+echo type your comment for this commit
+set /p comment=
+echo type your password for svn
+set /p pwd=
+E:
+cd DaiGou
+call mvn clean compile package -Dmaven.test.skip=true
+pause
+cd target
+HaoZipC x ljh-0.0.1-SNAPSHOT.war -oE:\DaiGou\target\api
+xcopy META-INF E:\pepec-web\META-INF\ /e /y /s
+xcopy WEB-INF E:\pepec-web\WEB-INF\ /e /y /s
+E:
+cd pepec-web
+svn add . --no-ignore --force
+svn commit -m '%commet%' --username lijinhuan --password %pwd% --no-auth-cache
+pause
