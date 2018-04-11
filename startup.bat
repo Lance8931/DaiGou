c0932d0c0ae3961521517fa81e254499ced733e0
Lance8931 <1838133252@qq.com>
2018-04-11 22:53:52 +0800

    update
    修改部署提交代码的bat文件

--- startup.bat
+++ startup.bat
@@ -5,13 +5,15 @@ set /p pwd=
 E:
 cd DaiGou
 call mvn clean compile package -Dmaven.test.skip=true
-pause
 cd target
 HaoZipC x ljh-0.0.1-SNAPSHOT.war -oE:\DaiGou\target\api
-xcopy META-INF E:\daigou-web\META-INF\ /e /y /s
-xcopy WEB-INF E:\daigou-web\WEB-INF\ /e /y /s
-xcopy org E:\daigou-web\org /e /y /s
-E:
+xcopy api\META-INF E:\daigou-web\META-INF\ /e /y /s
+xcopy api\WEB-INF E:\daigou-web\WEB-INF\ /e /y /s
+xcopy api\org E:\daigou-web\org /e /y /s
+cd ..
+cd ..
+cd ..
+cd ..
 cd daigou-web
 svn add . --no-ignore --force
 svn commit -m '%commet%' --username lijinhuan --password %pwd% --no-auth-cache
